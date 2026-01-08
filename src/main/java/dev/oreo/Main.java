b1c01d12e8bd32938b47a8d8f63099c793897e14
elias <elias.saire@ynov.com>
2026-01-08 16:40:49 +0100

    Add MongoDB

--- src/main/java/dev/oreo/Main.java
+++ src/main/java/dev/oreo/Main.java
@@ -9,20 +9,17 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 public class Main {
 
-    // 1) / affiche Hello World + variable d'env
     @GetMapping("/")
     public String home() {
         String msg = System.getenv().getOrDefault("MY_MESSAGE", "MY_MESSAGE not set");
         return "Hello World!<br/>MY_MESSAGE = " + msg;
     }
 
-    // 2) Route dynamique /hello/{name}
     @GetMapping("/hello/{name}")
     public String helloName(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
-    // 3) POST (ex: via un formulaire) -> redirige vers /hello/{name}
     @PostMapping("/hello")
     public RedirectView helloPost(@RequestParam String name) {
         return new RedirectView("/hello/" + name);
