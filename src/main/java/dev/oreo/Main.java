64e8dc2cd7fab1e040ec798e20bf884f65f236c5
elias <elias.saire@ynov.com>
2026-01-08 14:57:35 +0100

    Add env var + hello route + form

--- src/main/java/dev/oreo/Main.java
+++ src/main/java/dev/oreo/Main.java
@@ -2,16 +2,30 @@ package dev.oreo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @RestController
 public class Main {
 
+    // 1) / affiche Hello World + variable d'env
     @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
+    public String home() {
+        String msg = System.getenv().getOrDefault("MY_MESSAGE", "MY_MESSAGE not set");
+        return "Hello World!<br/>MY_MESSAGE = " + msg;
+    }
+
+    // 2) Route dynamique /hello/{name}
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    // 3) POST (ex: via un formulaire) -> redirige vers /hello/{name}
+    @PostMapping("/hello")
+    public RedirectView helloPost(@RequestParam String name) {
+        return new RedirectView("/hello/" + name);
     }
 
     public static void main(String[] args) {
