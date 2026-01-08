929fcffa8c2d7ad8f62ca3092e84f05f9c6bee08
elias <elias.saire@ynov.com>
2026-01-08 14:01:05 +0100

    edit branch dev read me

--- src/main/java/dev/oreo/Main.java
+++ src/main/java/dev/oreo/Main.java
@@ -1,7 +1,20 @@
 package dev.oreo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        SpringApplication.run(Main.class, args);
     }
 }
